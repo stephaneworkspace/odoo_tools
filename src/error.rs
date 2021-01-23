b327eba46dadf16212665d2d2dc2bf8264806cca
stephaneworkspace <s.bressani@bluewin.ch>
2021-01-23 13:52:09 +0100

    simplify

---
+++ src/error.rs
@@ -0,0 +1,62 @@
+use std::borrow::Cow;
+use std::borrow::Cow::Borrowed;
+use std::io;
+use xmlrpc::{Error as RequestError, Fault};
+
+pub(crate) const E_INV_CRED: Error = Error::Ost(Borrowed("invalid credential"));
+pub(crate) const E_INV_RESP: Error = Error::Ost(Borrowed("invalid xml-rpc response"));
+
+#[derive(Debug)]
+pub(crate) enum Error {
+    Io(io::Error),
+    Ost(Cow<'static, str>),
+    XmlRpcRequest(RequestError),
+    XmlRpcFault(Fault),
+    Reqwest(reqwest::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::Io(e)
+    }
+}
+
+impl From<&'static str> for Error {
+    fn from(e: &'static str) -> Error {
+        Error::Ost(e.into())
+    }
+}
+
+impl From<RequestError> for Error {
+    fn from(e: RequestError) -> Error {
+        Error::XmlRpcRequest(e)
+    }
+}
+
+impl From<Fault> for Error {
+    fn from(e: Fault) -> Error {
+        Error::XmlRpcFault(e)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Error {
+        Error::Reqwest(e)
+    }
+}
+
+pub(crate) fn print_err(err: String) {
+    eprintln!("{}", err);
+}
+
+pub(crate) fn print_if_err<T>(res: &Result<T, Error>) {
+    if let Err(ref err) = res {
+        match err {
+            Error::Ost(ref e) => eprintln!("{}", e.to_string()),
+            Error::Io(ref e) => eprintln!("{}", e.to_string()),
+            Error::XmlRpcRequest(ref e) => eprintln!("{}", e.to_string()),
+            Error::XmlRpcFault(ref e) => eprintln!("{}", e.to_string()),
+            Error::Reqwest(ref e) => eprintln!("{}", e.to_string()),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,82 +2,17 @@
 
 extern crate xmlrpc;
 
+mod error;
+use error::Error;
+use error::E_INV_CRED;
+//use error::E_INV_RESP;
 mod cfg;
 use cfg::parse;
 use xmlrpc::{Request, Value};
 
 use std::collections::BTreeMap;
 /// To simplify definitions using the XML-RPC "struct" type
-type OstDataMap = BTreeMap<String, Value>;
-
-// errors TODO in another file
-use std::borrow::Cow;
-use std::borrow::Cow::Borrowed;
-use std::io;
-use xmlrpc::{Error as RequestError, Fault};
-
-/// A commonly used Error
-pub(crate) const E_INV_CRED: Error = Error::Ost(Borrowed("invalid credential"));
-pub(crate) const E_INV_RESP: Error = Error::Ost(Borrowed("invalid xml-rpc response"));
-/// All the errors that can occur
-#[derive(Debug)]
-pub(crate) enum Error {
-    Io(io::Error),
-    Ost(Cow<'static, str>),
-    XmlRpcRequest(RequestError),
-    XmlRpcFault(Fault),
-    Reqwest(reqwest::Error),
-}
-
-/// Converting all sub-errors into Error.
-
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Error {
-        Error::Io(e)
-    }
-}
-
-impl From<&'static str> for Error {
-    fn from(e: &'static str) -> Error {
-        Error::Ost(e.into())
-    }
-}
-
-impl From<RequestError> for Error {
-    fn from(e: RequestError) -> Error {
-        Error::XmlRpcRequest(e)
-    }
-}
-
-impl From<Fault> for Error {
-    fn from(e: Fault) -> Error {
-        Error::XmlRpcFault(e)
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(e: reqwest::Error) -> Error {
-        Error::Reqwest(e)
-    }
-}
-
-/// Prints an error to stderr
-pub(crate) fn print_err(err: String) {
-    eprintln!("{}", err);
-}
-
-/// If the input is an Error then prints it to stderr
-pub(crate) fn print_if_err<T>(res: &Result<T, Error>) {
-    if let Err(ref err) = res {
-        match err {
-            Error::Ost(ref e) => eprintln!("{}", e.to_string()),
-            Error::Io(ref e) => eprintln!("{}", e.to_string()),
-            Error::XmlRpcRequest(ref e) => eprintln!("{}", e.to_string()),
-            Error::XmlRpcFault(ref e) => eprintln!("{}", e.to_string()),
-            Error::Reqwest(ref e) => eprintln!("{}", e.to_string()),
-        }
-    }
-}
+//type OstDataMap = BTreeMap<String, Value>;
 
 fn main() {
     let clap = parse();
@@ -155,7 +90,7 @@ fn val_to_response_simple(v: &Value) -> Result<&Value, Error> {
         ))
     }
 }*/
-
+/*
 fn val_to_response_btree(v: &Value) -> Result<&OstDataMap, Error> {
     let resp = v.as_struct().ok_or(E_INV_RESP)?;
 
@@ -171,4 +106,4 @@ fn val_to_response_btree(v: &Value) -> Result<&OstDataMap, Error> {
             format!("xmlrpc request failed: {}", status).into(),
         ))
     }
-}
+}*/
