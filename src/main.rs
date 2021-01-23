f9f984b1a87739808431ce20a0ad505cb6fa3985
stephaneworkspace <s.bressani@bluewin.ch>
2021-01-23 13:45:57 +0100

    ok with rust this time

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use std::io;
 use xmlrpc::{Error as RequestError, Fault};
 
 /// A commonly used Error
+pub(crate) const E_INV_CRED: Error = Error::Ost(Borrowed("invalid credential"));
 pub(crate) const E_INV_RESP: Error = Error::Ost(Borrowed("invalid xml-rpc response"));
 /// All the errors that can occur
 #[derive(Debug)]
@@ -105,29 +106,57 @@ fn main() {
     */
 }
 
-fn login(url: &str, db: &str, username: &str, password: &str) -> Result<String, Error> {
-    let request_common: String = format!("https://{}/xmlrpc/2/common", url.clone());
-    let resp = Request::new(request_common.clone().as_str())
-        .arg(Value::Struct(
+fn login(url: &str, db: &str, username: &str, password: &str) -> Result<i32, Error> {
+    let request_common: String = format!("{}/xmlrpc/2/common", url.clone());
+    let resp = Request::new("authenticate")
+        .arg(db.clone())
+        .arg(username.clone())
+        .arg(password.clone())
+        .arg(Value::Nil)
+        /*.arg(Value::Struct(
             vec![
-                ("host".to_string(), Value::from(url.clone())),
                 ("db".to_string(), Value::from(db.clone())),
                 ("username".to_string(), Value::from(username.clone())),
                 ("password".to_string(), Value::from(password.clone())),
+                ("".to_string(), Value::Struct(BTreeMap::new())),
             ]
             .into_iter()
             .collect(),
-        ))
+        ))*/
         .call_url(request_common.as_str())?;
-
-    val_to_response(&resp)?
-        .get("version")
-        .and_then(Value::as_str)
-        .map(String::from)
-        .ok_or(E_INV_RESP)
+    match resp.as_i32() {
+        Some(value) => Ok(value),
+        None => Err(E_INV_CRED),
+    }
+    //println!("{:?}", resp);
+    /*
+        val_to_response_simple(&resp)?
+            .get("authenticate")
+            .and_then(Value::as_i32)
+            .map(i32::from)
+            //.map(String::from)
+            .ok_or(E_INV_RESP)
+    */
 }
+/*
+fn val_to_response_simple(v: &Value) -> Result<&Value, Error> {
+    let resp = v.as_struct().ok_or(E_INV_RESP)?;
 
-fn val_to_response(v: &Value) -> Result<&OstDataMap, Error> {
+    let status = resp
+        .get("status")
+        .and_then(Value::as_str)
+        .ok_or(E_INV_RESP)?;
+
+    if status.starts_with("200") {
+        Ok(v)
+    } else {
+        Err(Error::Ost(
+            format!("xmlrpc request failed: {}", status).into(),
+        ))
+    }
+}*/
+
+fn val_to_response_btree(v: &Value) -> Result<&OstDataMap, Error> {
     let resp = v.as_struct().ok_or(E_INV_RESP)?;
 
     let status = resp
